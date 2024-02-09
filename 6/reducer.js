e4674030ec264e74dfdf13fe5e18bf0c9f12e989
ddomhall <domjshall@gmail.com>
2024-02-09 06:00:19 +0000

    reducer actions

--- 6/reducer.js
+++ 6/reducer.js
@@ -1,17 +1,21 @@
 import {createStore} from 'redux'
 
 const counterReducer = (state = 0, action) => {
-	switch (action) {
-		case 'INCREMENT':
-			return state + 1
-		break;
-		case 'DECREMENT':
-			return state - 1
-		break
-		default:
-		break;
-	}
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
 }
 
 const store = createStore(counterReducer)
-console.log(store)
+
+store.subscribe(() => {
+	console.log(store.getState())
+})
+
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'DECREMENT'})
