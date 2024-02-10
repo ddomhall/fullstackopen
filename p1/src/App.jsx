77b1cbad2928090e6fdff88c8abcb90b9c7bdd95
ddomhall <domjshall@gmail.com>
2024-02-10 04:45:41 +0000

    1.1

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -1,9 +1,42 @@
-const App = () => {
+export default function App() {
+    const course = 'Half Stack application development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
+
     return (
         <div>
-            <p>hello world</p>
+            <Header course={course}/>
+            <Content parts={parts} />
+            <Footer parts={parts}/>
         </div>
     )
 }
 
-export default App
+function Header({course}) {
+    return <h1>{course}</h1>
+}
+
+function Content({parts}) {
+    return (
+        <div>
+            {parts.map(p => <p>{p.name}: {p.exercises}</p>)}
+        </div>
+    )
+}
+
+function Footer({parts}) {
+    return <footer>Number of exercises: {parts.reduce((prev, cur) => prev + cur.exercises, 0)}</footer>
+}
+
