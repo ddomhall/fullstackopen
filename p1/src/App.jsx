ace4513ece41d56f81c1b83cfadd6939e5321944
ddomhall <domjshall@gmail.com>
2024-02-10 05:05:18 +0000

    1.5

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -1,38 +1,40 @@
 export default function App() {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
-        <div>
+        <>
             <Header course={course}/>
-            <Content parts={parts} />
-            <Footer parts={parts}/>
-        </div>
+            <Content course={course}/>
+            <Footer course={course}/>
+        </>
     )
 }
 
 function Header({course}) {
-    return <h1>{course}</h1>
+    return <h1>{course.name}</h1>
 }
 
-function Content({parts}) {
+function Content({course}) {
     return (
-        <div>
-            {parts.map(p => <Part part={p}/>)}
-        </div>
+        <>
+            {course.parts.map(p => <Part part={p}/>)}
+        </>
     )
 }
 
@@ -40,7 +42,7 @@ function Part({part}) {
     return <p>{part.name}: {part.exercises}</p>
 }
 
-function Footer({parts}) {
-    return <footer>Number of exercises: {parts.reduce((prev, cur) => prev + cur.exercises, 0)}</footer>
+function Footer({course}) {
+    return <footer>Number of exercises: {course.parts.reduce((prev, cur) => prev + cur.exercises, 0)}</footer>
 }
 
