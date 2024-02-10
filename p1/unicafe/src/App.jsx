72c420ac152297dcd1d9a2e12f7a20cce396eb5c
ddomhall <domjshall@gmail.com>
2024-02-10 08:27:06 +0000

    1.10

--- p1/unicafe/src/App.jsx
+++ p1/unicafe/src/App.jsx
@@ -9,12 +9,16 @@ export default function App() {
     return (
         <>
             <h1>give feedback</h1>
-            <button onClick={() => setGood(good + 1)}>good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button onClick={() => setBad(bad + 1)}>bad</button>
+            <Button text='good' onClick={() => setGood(good + 1)} />
+            <Button text='neutral' onClick={() => setNeutral(neutral + 1)} />
+            <Button text='bad' onClick={() => setBad(bad + 1)} />
             <Statistics good={good} neutral={neutral} bad={bad}/>
         </>
     )
+}
+
+function Button({text, onClick}) {
+    return <button onClick={onClick}>{text}</button>
 }
 
 function Statistics({good, neutral, bad}) {
@@ -22,14 +26,18 @@ function Statistics({good, neutral, bad}) {
         <>
             {good == 0 && neutral == 0 && bad == 0 ? <p>no feedback given</p> :
                 <div>
-                    <p>good: {good}</p>
-                    <p>neutral: {neutral}</p>
-                    <p>bad: {bad}</p>
-                    <p>total: {good + neutral + bad}</p>
-                    <p>average: {(good + neutral + bad) / 3}</p>
-                    <p>positive: {good / (good + neutral + bad)}</p>
+                    <StatisticLine text='good' value={good} />
+                    <StatisticLine text='neutral' value={neutral} />
+                    <StatisticLine text='bad' value={bad} />
+                    <StatisticLine text='total' value={good + neutral + bad} />
+                    <StatisticLine text='average' value={(good + neutral + bad) / 3} />
+                    <StatisticLine text='positive' value={good / (good + neutral + bad)} />
                 </div>
             }
         </>
     )
+}
+
+function StatisticLine({text, value}) {
+    return <p>{text}: {value}</p>
 }
