9b090a3fad885e6e450df4fca9986cbb307a874a
ddomhall <domjshall@gmail.com>
2024-02-10 18:06:09 +0000

    3.4

--- p3/phonebook/app.js
+++ p3/phonebook/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -39,6 +39,11 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    persons = persons.filter(p => p.id != Number(req.params.id))
+    res.status(204).end()
 })
 
 
