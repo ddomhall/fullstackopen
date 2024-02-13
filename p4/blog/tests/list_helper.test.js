ca2d769b69bfe8477c1a72c2ab240e56662f4623
ddomhall <domjshall@gmail.com>
2024-02-13 06:45:57 +0000

    4.5

--- p4/blog/tests/list_helper.test.js
+++ p4/blog/tests/list_helper.test.js
@@ -1,43 +1,42 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-    const blogs = []
+const listWithNoBlogs = []
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    }
+]
+const listWithTwoBlogs = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    }
+]
 
-    const result = listHelper.dummy(blogs)
-    expect(result).toBe(1)
+describe('dummy', () => {
+    test('dummy returns one', () => {
+        expect(listHelper.dummy([])).toBe(1)
+    })
 })
 
 describe('total likes', () => {
-    const listWithNoBlogs = []
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        }
-    ]
-    const listWithTwoBlogs = [
-        {
-            _id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
-            __v: 0
-        },
-        {
-            _id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
-            __v: 0
-        }
-    ]
-
     test('no blogs', () => {
         expect(listHelper.totalLikes(listWithNoBlogs)).toBe(0)
     })
@@ -51,3 +50,25 @@ describe('total likes', () => {
     })
 })
 
+describe('favourite blog', () => {
+    test('no blogs', () => {
+        expect(listHelper.favouriteBlog(listWithNoBlogs)).toEqual()
+    })
+
+    test('one blog', () => {
+        expect(listHelper.favouriteBlog(listWithOneBlog)).toEqual({
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5,
+        })
+    })
+
+    test('two blogs', () => {
+        expect(listHelper.favouriteBlog(listWithTwoBlogs)).toEqual({
+            title: "React patterns",
+            author: "Michael Chan",
+            likes: 7,
+        })
+    })
+
+})
--- p4/blog/utils/list_helper.js
+++ p4/blog/utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
     return blogs.length == 0 ? 0 : blogs.reduce((prev, cur) => prev + cur.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+    return blogs.reduce((prev, cur) => prev.likes >= cur.likes ? {
+        title: prev.title,
+        author: prev.author,
+        likes: prev.likes
+    } : {
+            title: cur.title,
+            author: cur.author,
+            likes: cur.likes
+        }, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+    return 0
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
