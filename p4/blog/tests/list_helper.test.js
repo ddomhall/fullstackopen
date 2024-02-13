f1e59f5dd9f3de4ff736211c0fc9a692d8e4c869
ddomhall <domjshall@gmail.com>
2024-02-13 05:57:09 +0000

    4.4

--- p4/blog/tests/list_helper.test.js
+++ p4/blog/tests/list_helper.test.js
@@ -1,8 +1,53 @@
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-  const blogs = []
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithNoBlogs = []
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    const listWithTwoBlogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('no blogs', () => {
+        expect(listHelper.totalLikes(listWithNoBlogs)).toBe(0)
+    })
+
+    test('one blog', () => {
+        expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+    })
+
+    test('two blogs', () => {
+        expect(listHelper.totalLikes(listWithTwoBlogs)).toBe(12)
+    })
+})
+
--- p4/blog/utils/list_helper.js
+++ p4/blog/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.length == 0 ? 0 : blogs.reduce((prev, cur) => prev + cur.likes, 0)
+}
+
 module.exports = {
-  dummy
+    dummy,
+    totalLikes
 }
