6ce967b0558c4295e67608cfa308d67a5122fa3b
ddomhall <domjshall@gmail.com>
2024-02-14 05:59:59 +0000

    4.6

--- p4/blog/tests/list_helper.test.js
+++ p4/blog/tests/list_helper.test.js
@@ -1,6 +1,7 @@
 const listHelper = require('../utils/list_helper')
 
 const listWithNoBlogs = []
+
 const listWithOneBlog = [
     {
         _id: '5a422aa71b54a676234d17f8',
@@ -11,6 +12,7 @@ const listWithOneBlog = [
         __v: 0
     }
 ]
+
 const listWithTwoBlogs = [
     {
         _id: "5a422a851b54a676234d17f7",
@@ -29,6 +31,58 @@ const listWithTwoBlogs = [
         __v: 0
     }
 ]
+
+const listWithSixBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+]
+
 
 describe('dummy', () => {
     test('dummy returns one', () => {
@@ -52,7 +106,7 @@ describe('total likes', () => {
 
 describe('favourite blog', () => {
     test('no blogs', () => {
-        expect(listHelper.favouriteBlog(listWithNoBlogs)).toEqual()
+        expect(listHelper.favouriteBlog(listWithNoBlogs)).toEqual(null)
     })
 
     test('one blog', () => {
@@ -70,5 +124,23 @@ describe('favourite blog', () => {
             likes: 7,
         })
     })
+})
 
+describe('most blogs', () => {
+    test('no blogs', () => {
+        expect(listHelper.mostBlogs(listWithNoBlogs)).toEqual(null)
+    })
+
+    test('one blog', () => {
+        expect(listHelper.mostBlogs(listWithOneBlog)).toEqual({
+            author: 'Edsger W. Dijkstra',
+            blogs: 1
+        })
+    })
+
+    test('six blogs', () => {
+        expect(listHelper.mostBlogs(listWithSixBlogs)).toEqual({
+            author: 'Robert C. Martin', blogs: 3 
+        })
+    })
 })
--- p4/blog/utils/list_helper.js
+++ p4/blog/utils/list_helper.js
@@ -1,3 +1,56 @@
+const {countBy} = require('lodash')
+
+const test = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+]
+
 const dummy = (blogs) => {
     return 1
 }
@@ -7,7 +60,7 @@ const totalLikes = (blogs) => {
 }
 
 const favouriteBlog = (blogs) => {
-    return blogs.reduce((prev, cur) => prev.likes >= cur.likes ? {
+    const favourite =  blogs.reduce((prev, cur) => prev.likes >= cur.likes ? {
         title: prev.title,
         author: prev.author,
         likes: prev.likes
@@ -16,14 +69,17 @@ const favouriteBlog = (blogs) => {
             author: cur.author,
             likes: cur.likes
         }, blogs[0])
+    return favourite ? favourite : null
 }
 
 const mostBlogs = (blogs) => {
-    return 0
+    const author = Object.entries(countBy(blogs, 'author')).reduce((prev, cur) => cur[1] > prev[1] ? cur : prev,[, 0])
+    return author[0] == undefined ? null : {author: author[0], blogs: author[1]}
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs
 }
