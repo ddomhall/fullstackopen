b208bd1f826135feb7b7a9210fbfab948e3706c2
ddomhall <domjshall@gmail.com>
2024-02-18 04:55:16 +0000

    5.1

--- p5/bloglist-frontend/src/App.jsx
+++ p5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,42 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [user, setUser] = useState()
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then(blogs =>
+            setBlogs( blogs )
+        )  
+    }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+    function handleSubmit(e) {
+        e.preventDefault()
+        loginService.login({username: e.target.username.value, password: e.target.password.value}).then(res => setUser(res))
+    }
+
+    return (
+        <>
+            <button onClick={() => setUser(!user)}>test</button>
+            {user ? 
+                <>
+                    <h2>blogs</h2>
+                    {blogs.map(blog =>
+                        <Blog key={blog.id} blog={blog} />
+                    )}
+                </> : <>
+                    <h2>log in</h2>
+                    <form onSubmit={handleSubmit}>
+                        <input name='username' placeholder='username' />
+                        <input name='password' placeholder='password' />
+                        <input type='submit' value='log in' />
+                    </form>
+                </>}
+        </>
+    )
 }
 
 export default App
