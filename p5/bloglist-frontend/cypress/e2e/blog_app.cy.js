86d44669773d1137f93e21801ba1c056700b61ec
ddomhall <domjshall@gmail.com>
2024-02-21 18:59:46 +0000

    cypress working?

--- p5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ p5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,11 @@
 describe('Blog app', function() {
-  beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:5173')
-  })
+    beforeEach(function() {
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.visit('http://localhost:5173')
+    })
 
-  it('Login form is shown', function() {
-    cy.get('show')
-  })
+    it('Login form is shown', function() {
+        cy.contains('blogs')
+    })
 })
+
