23acb8890eafe2a1816c3f6a22558c445a7f81a8
ddomhall <domjshall@gmail.com>
2024-02-24 08:25:59 +0000

    6.12

--- p6/redux-anecdotes/src/App.jsx
+++ p6/redux-anecdotes/src/App.jsx
@@ -1,3 +1,4 @@
+import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
@@ -5,6 +6,7 @@ import Filter from './components/Filter'
 const App = () => {
     return (
         <div>
+            <Notification />
             <h2>Anecdotes</h2>
             <Filter />
             <AnecdoteList />
