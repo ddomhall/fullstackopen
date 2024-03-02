bafe95ca08cd4927d8bccda448ddc1e574987859
ddomhall <domjshall@gmail.com>
2024-03-02 05:20:40 +0000

    7.11

--- p7/blogapp/frontend/src/App.js
+++ p7/blogapp/frontend/src/App.js
@@ -1,14 +1,15 @@
 import { useState, useEffect, useRef } from "react";
 import Blog from "./components/Blog";
+import { useDispatch, useSelector } from "react-redux";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 import storageService from "./services/storage";
 import {
-    createInfo,
-    createError,
-    removeMessage,
+  createInfo,
+  createError,
+  removeMessage,
 } from "./reducers/messageReducer.js";
-import { useDispatch, useSelector } from "react-redux";
+import { setBlogs, appendBlog } from "./reducers/blogReducer.js";
 
 import LoginForm from "./components/Login";
 import NewBlog from "./components/NewBlog";
@@ -16,147 +17,129 @@ import Notification from "./components/Notification";
 import Togglable from "./components/Togglable";
 
 const App = () => {
-    const [blogs, setBlogs] = useState([]);
-    const [user, setUser] = useState("");
-    //const [info, setInfo] = useState({ message: null });
+  const [user, setUser] = useState("");
 
-    const blogFormRef = useRef();
-    const dispatch = useDispatch();
+  const blogFormRef = useRef();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const user = storageService.loadUser();
-        setUser(user);
-    }, []);
+  useEffect(() => {
+    const user = storageService.loadUser();
+    setUser(user);
+  }, []);
 
-    useEffect(() => {
-        blogService.getAll().then((blogs) => setBlogs(blogs));
-    }, []);
+  useEffect(() => {
+    blogService.getAll().then((blogs) => dispatch(setBlogs(blogs)));
+  }, []);
 
-    const info = useSelector((store) => store.messages);
+  const info = useSelector((store) => store.messages);
+  const blogs = useSelector((store) => store.blogs);
 
-    //const //notifyWith = (message, type = "info") => {
-    //  setInfo({
-    //    message,
-    //    type,
-    //  });
-
-    //  setTimeout(() => {
-    //    setInfo({ message: null });
-    //  }, 3000);
-    //};
-
-    const login = async (username, password) => {
-        try {
-            const user = await loginService.login({ username, password });
-            setUser(user);
-            storageService.saveUser(user);
-            dispatch(createInfo("welcome!"));
-            setTimeout(() => {
-                dispatch(removeMessage());
-            }, 3000);
-        } catch (e) {
-            dispatch(createError("wrong username or password"));
-            setTimeout(() => {
-                dispatch(removeMessage());
-            }, 3000);
-        }
-    };
-
-    const logout = async () => {
-        setUser(null);
-        storageService.removeUser();
-        dispatch(createInfo("logged out"));
-        setTimeout(() => {
-            dispatch(removeMessage());
-        }, 3000);
-    };
-
-    const createBlog = async (newBlog) => {
-        const createdBlog = await blogService.create(newBlog);
-        dispatch(
-            createInfo(
-                `A new blog '${newBlog.title}' by '${newBlog.author}' added`,
-            ),
-        );
-        setTimeout(() => {
-            dispatch(removeMessage());
-        }, 3000);
-        setBlogs(blogs.concat(createdBlog));
-        blogFormRef.current.toggleVisibility();
-    };
-
-    const like = async (blog) => {
-        const blogToUpdate = {
-            ...blog,
-            likes: blog.likes + 1,
-            user: blog.user.id,
-        };
-        const updatedBlog = await blogService.update(blogToUpdate);
-        dispatch(
-            createInfo(
-                `A like for the blog '${blog.title}' by '${blog.author}'`,
-            ),
-        );
-        setTimeout(() => {
-            dispatch(removeMessage());
-        }, 3000);
-        setBlogs(blogs.map((b) => (b.id === blog.id ? updatedBlog : b)));
-    };
-
-    const remove = async (blog) => {
-        const ok = window.confirm(
-            `Sure you want to remove '${blog.title}' by ${blog.author}`,
-        );
-        if (ok) {
-            await blogService.remove(blog.id);
-            dispatch(
-                createInfo(
-                    `The blog' ${blog.title}' by '${blog.author} removed`,
-                ),
-            );
-            setTimeout(() => {
-                dispatch(removeMessage());
-            }, 3000);
-            setBlogs(blogs.filter((b) => b.id !== blog.id));
-        }
-    };
-
-    if (!user) {
-        return (
-            <div>
-                <h2>log in to application</h2>
-                <Notification info={info} />
-                <LoginForm login={login} />
-            </div>
-        );
+  const login = async (username, password) => {
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      storageService.saveUser(user);
+      dispatch(createInfo("welcome!"));
+      setTimeout(() => {
+        dispatch(removeMessage());
+      }, 3000);
+    } catch (e) {
+      dispatch(createError("wrong username or password"));
+      setTimeout(() => {
+        dispatch(removeMessage());
+      }, 3000);
     }
+  };
 
-    const byLikes = (b1, b2) => b2.likes - b1.likes;
+  const logout = async () => {
+    setUser(null);
+    storageService.removeUser();
+    dispatch(createInfo("logged out"));
+    setTimeout(() => {
+      dispatch(removeMessage());
+    }, 3000);
+  };
 
-    return (
-        <div>
-            <h2>blogs</h2>
-            <Notification info={info} />
-            <div>
-                {user.name} logged in
-                <button onClick={logout}>logout</button>
-            </div>
-            <Togglable buttonLabel="new note" ref={blogFormRef}>
-                <NewBlog createBlog={createBlog} />
-            </Togglable>
-            <div>
-                {blogs.sort(byLikes).map((blog) => (
-                    <Blog
-                        key={blog.id}
-                        blog={blog}
-                        like={() => like(blog)}
-                        canRemove={user && blog.user.username === user.username}
-                        remove={() => remove(blog)}
-                    />
-                ))}
-            </div>
-        </div>
+  const createBlog = async (newBlog) => {
+    const createdBlog = await blogService.create(newBlog);
+    dispatch(
+      createInfo(`A new blog '${newBlog.title}' by '${newBlog.author}' added`),
     );
+    setTimeout(() => {
+      dispatch(removeMessage());
+    }, 3000);
+    dispatch(appendBlog(createdBlog));
+    blogFormRef.current.toggleVisibility();
+  };
+
+  const like = async (blog) => {
+    const blogToUpdate = {
+      ...blog,
+      likes: blog.likes + 1,
+      user: blog.user.id,
+    };
+    const updatedBlog = await blogService.update(blogToUpdate);
+    dispatch(
+      createInfo(`A like for the blog '${blog.title}' by '${blog.author}'`),
+    );
+    setTimeout(() => {
+      dispatch(removeMessage());
+    }, 3000);
+    setBlogs(blogs.map((b) => (b.id === blog.id ? updatedBlog : b)));
+  };
+
+  const remove = async (blog) => {
+    const ok = window.confirm(
+      `Sure you want to remove '${blog.title}' by ${blog.author}`,
+    );
+    if (ok) {
+      await blogService.remove(blog.id);
+      dispatch(
+        createInfo(`The blog' ${blog.title}' by '${blog.author} removed`),
+      );
+      setTimeout(() => {
+        dispatch(removeMessage());
+      }, 3000);
+      setBlogs(blogs.filter((b) => b.id !== blog.id));
+    }
+  };
+
+  if (!user) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <Notification info={info} />
+        <LoginForm login={login} />
+      </div>
+    );
+  }
+
+  const byLikes = (b1, b2) => b2.likes - b1.likes;
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Notification info={info} />
+      <div>
+        {user.name} logged in
+        <button onClick={logout}>logout</button>
+      </div>
+      <Togglable buttonLabel="new note" ref={blogFormRef}>
+        <NewBlog createBlog={createBlog} />
+      </Togglable>
+      <div>
+        {blogs.sort(byLikes).map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            like={() => like(blog)}
+            canRemove={user && blog.user.username === user.username}
+            remove={() => remove(blog)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- p7/blogapp/frontend/src/index.js
+++ p7/blogapp/frontend/src/index.js
@@ -4,10 +4,12 @@ import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import messageReducer from "./reducers/messageReducer.js";
+import blogReducer from "./reducers/blogReducer.js";
 
 const store = configureStore({
   reducer: {
     messages: messageReducer,
+    blogs: blogReducer,
   },
 });
 
