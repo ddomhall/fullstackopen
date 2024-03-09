97815aba4ad2b6bef6795d7d091b6b32cb44f85c
ddomhall <domjshall@gmail.com>
2024-03-09 04:49:40 +0000

    8.8

---
+++ p8/library-frontend/src/components/Authors.js
@@ -0,0 +1,39 @@
+import { ALL_AUTHORS } from '../queries.js'
+import { useQuery } from '@apollo/client'
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <button onClick={() => console.log(result)}>test</button>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th>name</th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {result.data.allAuthors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
